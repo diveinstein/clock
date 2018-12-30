7eef9e0027cb7a00503888245395e47c27d744ae
Mark Sgarlato <sgarlato@gmail.com>
2018-12-30 10:57:27 -0800

    added stubs for macOS app with no xib or storyboard

--- Sovereignty/Sovereignty/Supporting Files/AppDelegate.swift
+++ Sovereignty/Sovereignty/Supporting Files/AppDelegate.swift
@@ -24,4 +24,50 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
 
 }
-
+//sample from stackexchange for app with no xib or stryboard
+/*
+ 
+ The main file must have
+ 
+ import Cocoa
+ 
+ let delegate = AppDelegate()
+ NSApplication.shared.delegate = delegate
+ 
+ NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
+ 
+ The AppDelegate must be
+ 
+ import Cocoa
+ 
+ 
+ class AppDelegate: NSObject, NSApplicationDelegate {
+ var newWindow: NSWindow?
+ var controller: ViewController?
+ 
+ func applicationDidFinishLaunching(_ aNotification: Notification) {
+ newWindow = NSWindow(contentRect: NSMakeRect(10, 10, 300, 300), styleMask: .resizable, backing: .buffered, defer: false)
+ 
+ controller = ViewController()
+ let content = newWindow!.contentView! as NSView
+ let view = controller!.view
+ content.addSubview(view)
+ 
+ newWindow!.makeKeyAndOrderFront(nil)
+ }
+ }
+ 
+ hen you have a viewController
+ 
+ import Cocoa
+ 
+ class ViewController : NSViewController {
+ override func loadView() {
+ let view = NSView(frame: NSMakeRect(0,0,100,100))
+ view.wantsLayer = true
+ view.layer?.borderWidth = 2
+ view.layer?.borderColor = NSColor.red.cgColor
+ self.view = view
+ }
+ }
+ */

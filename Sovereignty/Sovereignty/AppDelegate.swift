ea978f307af5a49a655f2778e9bdee87b156cdf9
Mark Sgarlato <sgarlato@gmail.com>
2018-12-30 06:34:59 -0800

    First commit of server app code in swift for swarm robot engine

---
+++ Sovereignty/Sovereignty/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Sovereignty
+//
+//  Created by Mark Sgarlato on 12/30/18.
+//  Copyright © 2018 Mark Sgarlato. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
